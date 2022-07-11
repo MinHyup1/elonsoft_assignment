ca4e3e8cd0723abace623936755da9b96a4b8b5d
MinHyup1 <p01073027511@gmail.com>
2022-07-11 22:47:11 +0900

    [20220711]

---
+++ 2_mudgame/src/main/java/com/main/MudgameApplication.java
@@ -0,0 +1,20 @@
+package com.main;
+
+import java.util.Scanner;
+
+public class MudgameApplication {
+	
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.println("머드게임 을 시작합니다...");
+		System.out.println("캐릭터의 별칭을 입력해주세요.");
+		String name = sc.next();
+		System.out.println("당신의 캐릭터의 별칭은 \"" + name + "\"입니다." );
+		
+		
+		
+	}
+
+}
